e496f1ac4cdad8ae3fd29d5c53e459d56f88c5b2
Lenisha Mendonsa <lenishamendonsa9@gmail.com>
2025-11-19 00:26:38 +0530

    initial commit

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3079';
+const API_URL = "https://geodata-vision-backend.onrender.com";
 
 // Async thunks
 export const registerUser = createAsyncThunk(
