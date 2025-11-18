68d57017c8fdd371905652740054d325ff51c0cc
Lenisha Mendonsa <lenishamendonsa9@gmail.com>
2025-11-19 00:38:36 +0530

    fix: update API URLs for production deployment

--- src/store/dataInputSlice.js
+++ src/store/dataInputSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3079';
+const API_URL = 'https://geodata-vision-backend.onrender.com';
 
 export const fetchUserCountryData = createAsyncThunk(
   'dataInput/fetchUserCountryData',
